4c35864c5f9705a85ad727b6a59565270ed1cc76
GiorgosTs19 <geontsou52@gmail.com>
2023-08-15 06:45:47 +0300

    Update DinnerReservationLong.jsx

--- resources/js/Pages/Admin/Reservations/ReservationViews/DinnerReservationLong.jsx
+++ resources/js/Pages/Admin/Reservations/ReservationViews/DinnerReservationLong.jsx
@@ -81,13 +81,13 @@ export function DinnerReservationLong() {
             </Row>
             <Row className={'my-4 mt-xxl-2'}>
                 <Col className={'border border-gray-400 mb-2 my-xxl-0 box_shadow rounded-4'} xs={12} xxl={7}>
-                    <p className={'p-1 my-1 user-select-none'}><i>Κράτηση για : {Date}</i></p>
-                    <p className={'p-1 my-1 user-select-none'}><i>Καταχωρήθηκε στις : {created_at(Placed_At)}</i></p>
-                    <p className={'p-1 my-1 user-select-none'}><i>Τελευταία αλλαγή : {hasChanges ? created_at(Updated_At) : '-'}</i></p>
+                    <p className={'p-1 my-1 user-select-none'}><i>Κράτηση <b>{activeReservation.Type === 'Dinner' ? 'Δείπνου' : 'Ξαπλώστρας'}</b> για τις {Date}</i></p>
                     <p className={'p-1 my-1 user-select-none'}>Κράτηση για <b>{Attendees.length + 1}</b> {Attendees.length === 0 ? 'άτομο' : 'άτομα'}.</p>
+                    <p className={'p-1 my-1 user-select-none'}><i>Καταχωρήθηκε στις  {created_at(Placed_At)}</i></p>
+                    <p className={'p-1 my-1 user-select-none'}><i>Τελευταία αλλαγή : {hasChanges ? created_at(Updated_At) : '-'}</i></p>
                 </Col>
-                <Col className={'d-flex mt-4 mt-xxl-2'} xs={12} xxl={5}>
-                        <h4 className={'border-bottom p-4 m-auto box_shadow rounded-4'}>Αρ. Κράτησης : {Confirmation_Number}</h4>
+                <Col className={'d-flex mt-4 mt-xxl-2 flex-column'} xs={12} xxl={5}>
+                    <h4 className={'border-bottom p-4 m-auto box_shadow rounded-4'}>Αρ. Κράτησης : {Confirmation_Number}</h4>
                 </Col>
             </Row>
             <Row className={'box_shadow mt-4 mt-xxl-2 border rounded-3'}>
